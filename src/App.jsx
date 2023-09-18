f49c461095b14ac6c972cf761cdb60359275f564
unknown <sharan.akash2511@gmail.com>
2023-09-19 00:12:27 +0530

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,89 @@
-import React, { useState } from 'react'
-import Dice from './Dice'
-import {nanoid} from "nanoid"
-import Confetti from 'react-confetti'
+import React, { useEffect, useState } from "react";
+import Dice from "./Dice";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 const App = () => {
-  
-  const [bestTimeNumber,setBestTimeNumber] = useState(Number.MAX_VALUE);
+  const [bestTimeNumber, setBestTimeNumber] = useState(Number.MAX_VALUE);
+  const [tenzies, setTenzies] = useState(false);
 
-  const [tenzies,setTenzies]=useState(false);
-  const generateNewDice=()=>{
+  const generateNewDice = () => {
     return {
-      value:Math.ceil(Math.random()*6),
-      isHeld:false,
-      id:nanoid() 
-    }
-  }
-  const allNewDice=()=>{
-    const newDice=[];
-    for(let i=0;i<10;i++)
-    {
-      newDice.push(generateNewDice())
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  };
+
+  const allNewDice = () => {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDice());
     }
     return newDice;
-  }
-  
-  const [dice, setDice]=useState(allNewDice());
-  React.useEffect(
-    ()=>{
-     const allHeld=dice.every(x=>x.isHeld) 
-    const firstValue=dice[0].value;
-    const allSameValue=dice.every(x=> x.value===firstValue)
-    if(allHeld && allSameValue)
-   { setTenzies(true)
-     console.log("You Won!")          
+  };
+
+  const [dice, setDice] = useState(allNewDice());
+
+  useEffect(() => {
+    const allHeld = dice.every((x) => x.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((x) => x.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+      console.log("You Won!");
     }
-    },[dice])
+  }, [dice]);
 
-   
-  const rollDice=()=>{
-    if(!tenzies)
-    {
-    setAttempts(attempts+1)
-    setDice(oldDice=>oldDice.map(x=>{
-            return x.isHeld? x: generateNewDice()
-    }))
-  }
-  else{
-    console.log(bestTimeNumber);
-    let x = Math.min(bestTimeNumber,attempts);
-    setBestTimeNumber(x);
-     console.log(x);
-    setTenzies(false)
-    setDice(allNewDice())
-   
-      //console.log(bestTimeNumber);
-      setAttempts(0)
-  }
-  // if(!tenzies)
-  //    setAttempts(attempts+1)
-  //   else
-  //   {
-  //     bestTimeNumber = Math.min(bestTimeNumber,attempts);
-  //     console.log(bestTimeNumber);
-  //     setAttempts(0)
+  //onlcick function
+  const [attempts, setAttempts] = useState(0);
+  const rollDice = (e) => {
+    e.preventDefault();
+    if (tenzies) {
+      setBestTimeNumber(Math.min(attempts, bestTimeNumber));
+      setAttempts(0);
+      setTenzies(false);
+      setDice(allNewDice());
+    } else {
+      setAttempts(attempts + 1);
+      setDice((oldDice) =>
+        oldDice.map((x) => {
+          return x.isHeld ? x : generateNewDice();
+        })
+      );
+    }
+  };
 
-  //   }
-    // if(tenzies && attempts<bestTime)
-    // {
-    //    setbestTime(attempts)
-    // }
-  }
-   const [attempts,setAttempts]=useState(0);
-  const holdDice=(id)=>{
-      setDice(oldDice=> oldDice.map(x=>{
-        return x.id===id?{...x,isHeld:!x.isHeld}:x
-      }))
-      
-  }
-  const diceElements=dice.map(x=> 
-  <Dice key={x.id} 
-  value={x.value}
-  isHeld={x.isHeld}
-  holdDice={()=>holdDice(x.id)}
-  />);
+  const holdDice = (id) => {
+    setDice((oldDice) =>
+      oldDice.map((x) => {
+        return x.id === id ? { ...x, isHeld: !x.isHeld } : x;
+      })
+    );
+  };
+  const diceElements = dice.map((x) => (
+    <Dice
+      key={x.id}
+      value={x.value}
+      isHeld={x.isHeld}
+      holdDice={() => holdDice(x.id)}
+    />
+  ));
   return (
     <main>
       {bestTimeNumber}
-      {tenzies && <Confetti/>}
-      <h1 className='title'>TENZIES</h1>
-      <p className='instructions'> Roll until all dice are same. click each die to freeze 
-      it at its current value between rolls.</p>
+      {tenzies && <Confetti />}
+      <h1 className="title">TENZIES</h1>
+      <p className="instructions">
+        Roll until all dice are same. click each die to freeze it at its current
+        value between rolls.
+      </p>
       <h3> LeastMoves:{bestTimeNumber}</h3>
-      <div className="dice-container">
-         {diceElements}
-    </div>
-    <button className='roll-dice' onClick={rollDice}> {tenzies? 'NewGame': 'Roll'} </button>
-    <h3>Attempts: {attempts}</h3>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        {tenzies ? "NewGame" : "Roll"}
+      </button>
+      <h3>Attempts: {attempts}</h3>
     </main>
-  )
-}
-export default App
+  );
+};
+export default App;
